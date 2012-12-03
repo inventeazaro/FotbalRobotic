c0cc917383e622a08bbb04571a10f6bfffcdd968
Ionut Cotoi <ionut.cotoi@opendevlabs.com>
2012-12-03 16:18:19 +0200

    C++ OpenCV Robot Tracking platform - Initial commit

---
+++ OpenCVExamples/CPP/qt-opencv-multithreaded-1.21/ColorCalibration.h
@@ -0,0 +1,31 @@
+#ifndef COLORCALIBRATION_H
+#define COLORCALIBRATION_H
+
+// Ball Threshold - Logitech Camera - Normal illumination
+#define BallColorLow        Scalar(170, 122, 88)
+#define BallColorHigh       Scalar(180, 188, 248)
+
+
+// Color Pads Thresholds
+#define ColorGreen_Low      Scalar(67, 103, 46)
+#define ColorGreen_High     Scalar(100, 209, 184)
+
+#define ColorRed_Low        Scalar(169, 168, 115)
+#define ColorRed_High       Scalar(180, 243, 200)
+
+#define ColorBlue_Low       Scalar(79, 186, 84)
+#define ColorBlue_High      Scalar(146, 243, 244)
+
+#define ColorYellow_Low     Scalar(19, 159, 81)
+#define ColorYellow_High    Scalar(56, 244, 255)
+
+#define ColorOrange_Low     Scalar(3, 110, 122)
+#define ColorOrange_High    Scalar(5, 233, 212)
+
+#define ColorDarkBlue_Low   Scalar(41, 40, 31)
+#define ColorDarkBlue_High  Scalar(141, 129, 80)
+
+#define ColorIndigo_Low     Scalar(137, 91, 43)
+#define ColorIndigo_High    Scalar(156, 233, 132)
+
+#endif // COLORCALIBRATION_H
