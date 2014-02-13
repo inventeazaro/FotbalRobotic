6d2678e522af0418605f5128e2d8939f3155a5fd
Bogdan Nistor <nistor.bogdan91@gmail.com>
2014-02-13 21:34:26 +0200

    Suport pentru 2 camere

--- TrackingServer/TrackingServer/main.cpp
+++ TrackingServer/TrackingServer/main.cpp
@@ -106,38 +106,42 @@ int main(int argc,char **argv)
         float MarkerSize=0.008;
         //read the input image
         cv::Mat InImage;
+        cv::Mat InImage2;
         //try opening first as video
         VideoCapture vreader(1);
+        VideoCapture vreader2(2);
+
         const char mainWindow[] = "FotbalRobotic Tracker";
         cv::namedWindow(mainWindow, CV_WINDOW_FULLSCREEN);
         cv::Mat HSVImage;
         cv::Mat BallMask;
+        cv::Mat HSVImage2;
+        cv::Mat BallMask2;
 
         int mid = 0;
         int rc = 0;
 
         MarkerSize=10;
 
-        vreader.set(CV_CAP_PROP_FRAME_WIDTH, 1080);
-        vreader.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+        //ATENTIE! AICI SETAM LA 1080 CU 720. IN CAMERA PARAMETERS MAI JOS AVEM 640 CU 480 PE CARE LE MODIFICAM LA 1080 CU 720??
+        //intrebare: de ce folosim camparam?
 
-        //vreader.open("http://admin:admin@192.168.20.36/oneshotimage.jpg");
+        vreader.set(CV_CAP_PROP_FRAME_WIDTH, 800);
+        vreader.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
 
+        vreader2.set(CV_CAP_PROP_FRAME_WIDTH, 800);
+        vreader2.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
 
         while(1) {
         if (vreader.isOpened()) {
             vreader.grab();
             vreader.retrieve(InImage);
-            //InImage=cv::imread(TEST_FILE);
         }
 
-//        cv::GaussianBlur(InImage, temp, cv::Size(0, 0), valSigmaX, valSigmaY);
-//        cv::addWeighted(InImage, 1.5, temp, -0.5, 0, InImage);
-          //cv::erode(InImage, InImage, Mat(), Point(-1,-1), 2);
-
-          //cv::dilate(InImage, InImage, 0, 2);
-
-        //cv::imshow("enhanced", temp);
+        if (vreader2.isOpened()) {
+            vreader2.grab();
+            vreader2.retrieve(InImage2);
+        }
 
         //at this point, we should have the image in InImage
         //if empty, exit
@@ -146,6 +150,10 @@ int main(int argc,char **argv)
             return 0;
         }
 
+        if (InImage2.total()==0) {
+            cerr<<"Could not open input"<<endl;
+            return 0;
+        }
 
         cv::createTrackbar("H Low", mainWindow, &hLow, 180, NULL);
         cv::createTrackbar("S Low", mainWindow, &sLow, 255, NULL);
@@ -158,78 +166,67 @@ int main(int argc,char **argv)
         cv::Scalar highThreshold(hHigh, sHigh, vHigh, 0);
 
         cv::cvtColor(InImage, HSVImage, CV_RGB2HSV);
+
+        cv::cvtColor(InImage2, HSVImage2, CV_RGB2HSV);
+
         cv::inRange(HSVImage, lowThreshold, highThreshold, BallMask);
-        //cv::GaussianBlur(BallMask, BallMask, cv::Size(1, 1), 2);
-        //cv::erode(BallMask, BallMask, Mat(), Point(-1,-1), 1);
-        //cv::dilate(BallMask, BallMask, Mat(), Point(-1,-1), 1);
 
-//        vector<Vec3f> circles;
+        cv::inRange(HSVImage2, lowThreshold, highThreshold, BallMask2);
 
-//        HoughCircles(BallMask, circles, CV_HOUGH_GRADIENT, 2, BallMask.rows, 70, 70, 0, 50);
-//        for( size_t i = 0; i < circles.size(); i++ )
-//        {
-//             Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-//             int radius = cvRound(circles[i][2]);
-//             // draw the circle center
-//             circle( InImage, center, 3, Scalar(0,255,0), -1, 8, 0 );
-//             // draw the circle outline
-//             circle( InImage, center, radius, Scalar(0,0,255), 3, 8, 0 );
-//        }
-//        CBlobResult blobs;
-//        CBlob blob;
-//        IplImage img = BallMask;
-//        blobs = CBlobResult( img, NULL, 255);
-//        // Determine Min & Max blob size
-//        blobs.Filter( blobs, B_INCLUDE, CBlobGetArea(), B_LESS, 599);
-//        blobs.Filter( blobs, B_INCLUDE, CBlobGetArea(), B_GREATER, 59);
-//        int totalBlobs = blobs.GetNumBlobs();
-
-//        for(int b=0; b<totalBlobs; b++)
-//        {
-//            blob = blobs.GetBlob(b);
-//            // ICOTOI blob size
-//            //cout<<"Blob: "<<b<<"BlobSize: "<<blob.Area()<<std::endl;
-
-//            // Draw rectangle around each blob
-//            rectangle(InImage, cvPoint(blob.MinX(), blob.MinY()),
-//            cvPoint(blob.MaxX(), blob.MaxY() ), CV_RGB(0,255,0), 1 );
-//        }
 
         Moments ballMoments = moments(BallMask, false);
+
+        Moments ballMoments2 = moments(BallMask2, false);
+
         Point   ballPosition;
 
-
         float ballArea = ballMoments.m00;
+        float ballArea2 = ballMoments2.m00;
+
         if(ballArea > 10) {
             ballPosition.x = ballMoments.m10/ballArea;
             ballPosition.y = ballMoments.m01/ballArea;
-
             //circle(InImage, ballPosition, 10, Scalar(255, 255, 255), 2);
             robotCoords coords;
-
             coords.id = 0; // Id Ball
             coords.x  =  ballPosition.x;
             coords.y  =  ballPosition.y;
             coords.angle = 0;
-            cerr<<ballPosition.x<<" "<<ballPosition.y<<endl;
+           // cerr<<ballPosition.x<<" "<<ballPosition.y<<endl;
             coords.timestamp = std::time(0);
             mosquitto_publish(mosq, &mid, "coords", sizeof(coords), &coords, 0, true);
         }
+        else
+            if(ballArea2 > 10) {
+                ballPosition.x = ballMoments2.m10/ballArea2;
+                ballPosition.y = ballMoments2.m01/ballArea2;
+                //circle(InImage, ballPosition, 10, Scalar(255, 255, 255), 2);
+                robotCoords coords;
+                coords.id = 0; // Id Ball
+                coords.x  =  ballPosition.x;
+                coords.y  =  ballPosition.y;
+                coords.angle = 0;
+                cerr<<ballPosition.x<<" "<<ballPosition.y<<endl;
+                coords.timestamp = std::time(0);
+                mosquitto_publish(mosq, &mid, "coords", sizeof(coords), &coords, 0, true);
+            }
 
-        //imshow("ceva", BallMask);
-
+        imshow("ceva", BallMask);
+        imshow("ceva2", BallMask2);
         //read camera parameters if specifed
-
-        CamParam.readFromXMLFile("/home/bogdan/dev/FotbalRobotic/TrackingServer/arutest/camera.yml");
+        CamParam.readFromXMLFile("/home/bogdan/dev/FotbalRobotic/TrackingServer/TrackingServer/camera.yml");
         //resizes the parameters to fit the size of the input image
         CamParam.resize( InImage.size());
         bitwise_not(InImage, InImage);
+        bitwise_not(InImage2, InImage2);
         //read marker size if specified
         //Ok, let's detect
         MDetector.getCandidates();
         MDetector.setMinMaxSize(0.005, 0.5);
 
         MDetector.detect(InImage,Markers,CamParam,MarkerSize);
+        MDetector.detect(InImage2,Markers,CamParam,MarkerSize); //este posibil sa fie bine asa, daca nu reinitializeaza vectorul markers. altfel va fi mai enervant
+
         //for each marker, draw info and its boundaries in the image
         for (unsigned int i=0;i<Markers.size();i++) {
             //cout<<Markers[i]<<endl;
@@ -277,13 +274,6 @@ int main(int argc,char **argv)
             mosquitto_reconnect(mosq);
         }
 
-        //draw a 3d cube in each marker if there is 3d info
-        //if (  CamParam.isValid() && MarkerSize!=-1)
-        //    for (unsigned int i=0;i<Markers.size();i++) {
-        //        CvDrawingUtils::draw3dCube(InImage,Markers[i],CamParam);
-        //    }
-        //show input with augmented information
-
         //Cerc verde 0,0
         circle(InImage, Point(15,30), 5, Scalar(0, 255, 255), 2);
 
@@ -303,9 +293,11 @@ int main(int argc,char **argv)
 
         cv::imshow("FotbalRobotic Tracker",InImage);
 
+        cv::imshow("Camera 2",InImage2);
+
         //show also the internal image resulting from the threshold operation
         //cv::imshow("thes", MDetector.getThresholdedImage() );
-        cv::waitKey(1);//wait for key to be pressed
+        cv::waitKey(99);//wait for key to be pressed
 
         }
 
