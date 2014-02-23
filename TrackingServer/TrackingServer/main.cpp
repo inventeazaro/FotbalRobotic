bd70b4cbf8419cd84db56de18f0e62f33d347d69
Bogdan Nistor <nistor.bogdan91@gmail.com>
2014-02-23 12:50:52 +0200

    adaugat scalare pe x pentru jumatate de teren

--- TrackingServer/TrackingServer/main.cpp
+++ TrackingServer/TrackingServer/main.cpp
@@ -43,7 +43,6 @@ or implied, of Rafael Muñoz Salinas.
 using namespace cv;
 using namespace aruco;
 
-
 int hLow = 116;
 int sLow = 73;
 int vLow = 99;
@@ -51,6 +50,16 @@ int hHigh = 141;
 int sHigh = 149;
 int vHigh = 169;
 
+//adaugat parametrii culoare verde
+int hLow_00 = 70;
+int sLow_00 = 89;
+int vLow_00 = 89;
+int hHigh_00 = 91;
+int sHigh_00 = 255;
+int vHigh_00 = 255;
+
+
+// TODO calcul coordonate relativ la dimensiunea fiecarei jumatati de teren
 
 int idAssocMat[1024] = {1};
 
@@ -111,12 +120,15 @@ int main(int argc,char **argv)
         VideoCapture vreader(1);
         VideoCapture vreader2(2);
 
+
         const char mainWindow[] = "FotbalRobotic Tracker";
         cv::namedWindow(mainWindow, CV_WINDOW_FULLSCREEN);
         cv::Mat HSVImage;
         cv::Mat BallMask;
         cv::Mat HSVImage2;
         cv::Mat BallMask2;
+        cv::Mat VerdeMask;
+        cv::Mat VerdeMask2;
 
         int mid = 0;
         int rc = 0;
@@ -125,22 +137,27 @@ int main(int argc,char **argv)
 
         //ATENTIE! AICI SETAM LA 1080 CU 720. IN CAMERA PARAMETERS MAI JOS AVEM 640 CU 480 PE CARE LE MODIFICAM LA 1080 CU 720??
         //intrebare: de ce folosim camparam?
-
         vreader.set(CV_CAP_PROP_FRAME_WIDTH, 800);
         vreader.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
-
         vreader2.set(CV_CAP_PROP_FRAME_WIDTH, 800);
         vreader2.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
 
+        //TODO important shit!
+        //system("guvcview -d /dev/video1 -f MJPEG -s 128x72 -o");
+        //system("guvcview -d /dev/video2 -f MJPEG -s 128x72 -o");
+
         while(1) {
+
         if (vreader.isOpened()) {
-            vreader.grab();
-            vreader.retrieve(InImage);
+//            vreader.grab();
+//            vreader.retrieve(InImage);
+            vreader>>InImage;
         }
 
         if (vreader2.isOpened()) {
-            vreader2.grab();
-            vreader2.retrieve(InImage2);
+//            vreader2.grab();
+//            vreader2.retrieve(InImage2);
+            vreader2>>InImage2;
         }
 
         //at this point, we should have the image in InImage
@@ -162,8 +179,11 @@ int main(int argc,char **argv)
         cv::createTrackbar("S High", mainWindow, &sHigh, 255, NULL);
         cv::createTrackbar("V High", mainWindow, &vHigh, 255, NULL);
 
-        cv::Scalar lowThreshold(hLow, sLow, vLow, 0);
+        cv::Scalar lowThreshold(hLow, sLow, vLow, 0);   //pt minge
         cv::Scalar highThreshold(hHigh, sHigh, vHigh, 0);
+
+        cv::Scalar lowThreshold_00(hLow_00, sLow_00, vLow_00, 0);   //pt verde
+        cv::Scalar highThreshold_00(hHigh_00, sHigh_00, vHigh_00, 0);
 
         cv::cvtColor(InImage, HSVImage, CV_RGB2HSV);
 
@@ -173,15 +193,27 @@ int main(int argc,char **argv)
 
         cv::inRange(HSVImage2, lowThreshold, highThreshold, BallMask2);
 
+        cv::inRange(HSVImage, lowThreshold_00, highThreshold_00, VerdeMask);   //varde mask
+
+        cv::inRange(HSVImage2, lowThreshold_00, highThreshold_00, VerdeMask2);   //varde mask
 
         Moments ballMoments = moments(BallMask, false);
 
         Moments ballMoments2 = moments(BallMask2, false);
 
+        Moments verdeMoments = moments(VerdeMask, false);
+
+        Moments verdeMoments2 = moments(VerdeMask2, false);
+
         Point   ballPosition;
+
+        Point   verdePosition, verdePosition2;
 
         float ballArea = ballMoments.m00;
         float ballArea2 = ballMoments2.m00;
+        float verdeArea = verdeMoments.m00;
+        float verdeArea2 = verdeMoments2.m00;
+        int cpsx=0, cpsy=0, cpdx=0, cpdy=0;
 
         if(ballArea > 10) {
             ballPosition.x = ballMoments.m10/ballArea;
@@ -211,8 +243,24 @@ int main(int argc,char **argv)
                 mosquitto_publish(mosq, &mid, "coords", sizeof(coords), &coords, 0, true);
             }
 
+        if (verdeArea > 20) {
+            verdePosition.x = verdeMoments.m10/verdeArea;
+            verdePosition.y = verdeMoments.m01/verdeArea;
+            circle(InImage, verdePosition, 27.3, Scalar(255, 255, 255), 2);
+        }
+
+        if (verdeArea2 > 20) {
+            verdePosition2.x = verdeMoments2.m10/verdeArea2;
+            verdePosition2.y = verdeMoments2.m01/verdeArea2;
+            circle(InImage, verdePosition, 27.3, Scalar(255, 255, 255), 2);
+        }
+
+        //facem regula de trei simpla astfel incat sa avem 0-500, 501-1000 coordonatele terenului.
+        //mai trebuie sa luam coordonatele portilor pentru a stii exact unde este 0 si respectiv 1000
+
         imshow("ceva", BallMask);
-        imshow("ceva2", BallMask2);
+        imshow("verde",VerdeMask);
+        //imshow("ceva2", BallMask2);
         //read camera parameters if specifed
         CamParam.readFromXMLFile("/home/bogdan/dev/FotbalRobotic/TrackingServer/TrackingServer/camera.yml");
         //resizes the parameters to fit the size of the input image
@@ -225,12 +273,62 @@ int main(int argc,char **argv)
         MDetector.setMinMaxSize(0.005, 0.5);
 
         MDetector.detect(InImage,Markers,CamParam,MarkerSize);
-        MDetector.detect(InImage2,Markers,CamParam,MarkerSize); //este posibil sa fie bine asa, daca nu reinitializeaza vectorul markers. altfel va fi mai enervant
+        //cum consider ca ar fi bine: fac de doua ori, dar a doua oara adaug un parametru la coordonate.
 
         //for each marker, draw info and its boundaries in the image
         for (unsigned int i=0;i<Markers.size();i++) {
             //cout<<Markers[i]<<endl;
             Markers[i].draw(InImage,Scalar(0,0,255),2);
+            double position[3];
+            double orientation[4];
+
+            cv::Point2f markerCenter, p1, p2;
+            double pd, angle;
+            robotCoords coords;
+
+            markerCenter = Markers[i].getCenter();
+            if (Markers[i].id != 1) {  //daca nu este markerul de langa poarta
+
+                coords.id = idAssocMat[Markers[i].id];
+                coords.x  = markerCenter.x;
+                coords.y  = markerCenter.y;
+
+                if (coords.x > cpsx && coords.x < verdePosition.x) {
+
+                    //scalare coordonate
+                    coords.x = (coords.x - cpsx) * 500 / (verdePosition.x - cpsx);
+
+                    p1.x = Markers[i][0].x;
+                    p1.y = Markers[i][0].y;
+                    p2.x = Markers[i][1].x;
+                    p2.y = Markers[i][1].y;
+
+                    pd = p1.y-p2.y/p1.x-p2.x;
+
+                    angle= atan2(p1.x-p2.x, p1.y-p2.y);
+                    angle = angle*180/3.1415 + 180;
+                    cout<<"Id:"<< coords.id << "Ang: "<<angle<<endl;
+
+                    coords.angle = angle;
+                    coords.timestamp = std::time(0);
+                    mosquitto_publish(mosq, &mid, "coords", sizeof(coords), &coords, 0, true);
+                }
+            }
+            else {
+                //salvam coordonatele portii vazute de camera 1, adica poarta din stanga
+                cpsx = markerCenter.x;
+                cpsy = markerCenter.y;
+            }
+        }
+
+        Markers.clear();
+
+        MDetector.detect(InImage2,Markers,CamParam,MarkerSize); //este posibil sa fie bine asa, daca nu reinitializeaza vectorul markers. altfel va fi mai enervant
+
+        //for each marker, draw info and its boundaries in the image
+        for (unsigned int i=0;i<Markers.size();i++) {
+            //cout<<Markers[i]<<endl;
+            Markers[i].draw(InImage2,Scalar(0,0,255),2);
             double position[3];
             double orientation[4];
 
